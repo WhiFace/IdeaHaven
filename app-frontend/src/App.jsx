980d9ad95f53168f7d20088eeef2c980cd587185
Benedict Gborkorquellie <benedictgborkorquellie@Benedicts-Air.lan>
2024-11-27 14:09:05 -0600

    Initial setup

---
+++ app-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl text-center text-gray-900'>Vite + React</h1>
+
+      <p className=' text-pretty text-base text-blue-950 text-center'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
